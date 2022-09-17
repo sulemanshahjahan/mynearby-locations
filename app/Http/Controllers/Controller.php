07e20c39f2de5c7fb677ac22782af67aa2adb567
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-09-17 23:26:52 +0500

    Second Commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,13 +1,68 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use Validator;
+use App\Models\User;
 
-class Controller extends BaseController
+class AuthController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function register(Request $request){
+        // validation
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password'
+        ]);
+
+        if($validator->fails()){
+            $response = [
+                'success' => false,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $input['c_password'] = bcrypt($input['c_password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
+
+        $response = [
+            'success' => true,
+            'data' => $success,
+            'message' => 'User register successfully'
+        ];
+
+        return response()->json($response,200);
+
+    }
+
+    public function login(Request $request){
+        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->plainTextToken;
+            $success['name'] = $user->name;
+
+            $response = [
+                'success' => true,
+                'data' => $success,
+                'message' => 'User login successfully'
+            ];
+            return response()->json($response,200);
+        }else{
+            $response = [
+                'success' => false,
+                'message' => 'Unauthorised'
+            ];
+            return response()->json($response);
+        }
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'c_password',
     ];
 
     /**
@@ -30,6 +31,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
+        'c_password',
         'remember_token',
     ];
 
