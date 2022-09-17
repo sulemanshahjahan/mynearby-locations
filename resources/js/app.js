dde7f41f9755175e360e85fc4246430f89e7a255
sulemanshahjahan <suleman.shahjahan@gmail.com>
2022-09-18 04:56:34 +0500

    see

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,13 @@ import './bootstrap';
 import { createApp } from 'vue';
 import router from './router.js'
 import store from './store'
+var cors = require('cors')
+
 
 import App from './layouts/App.vue'
 
 createApp(App)
     .use(router)
     .use(store)
+    .use(cors)
     .mount("#app")
